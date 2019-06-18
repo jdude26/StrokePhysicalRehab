25a0bc25b47f359b5231fc8cc6ad3b2dcbd9c29c
Jared Duval <ffilo26@aol.com>
2019-06-18 17:21:58 -0400

    Trying to build

--- PhysicalRehabilitation/Assets/Animations.cs
+++ PhysicalRehabilitation/Assets/Animations.cs
@@ -6,7 +6,7 @@ public class Animations : MonoBehaviour
 {
     // Start is called before the first frame update
     private Animator anim;
-    private string[] states = new string[] { "Welcome", "Sit", "HandHeart", "LiftElbow", "ExtendLeft", "Armrest", "SitInstructions", "ScootFeet", "LeanForward", "Stand", "ShoulderFeet", "LungeRight", "LungeLeft" };
+    private string[] states = new string[] { "Welcome", "Sit", "HandHeart", "LiftElbow", "ExtendLeft", "Armrest", "SitInstructions", "ScootFeet", "LeanForward", "Stand", "ShoulderFeet", "LungeLeft", "StepBackward", "LungeRight" };
     private int currentIndex = -1;
 
 
@@ -54,8 +54,104 @@ public class Animations : MonoBehaviour
                 {
                     currentIndex--;
                     anim.Play(states[currentIndex]);
+                    PlaySound(states[currentIndex]);
                 }
 
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (null != anim)
+            {
+                if (currentIndex == 2)
+                {
+                    PlaySound("HandHigher");
+                }
+                else if (currentIndex == 3)
+                {
+                    PlaySound("ElbowHigher");
+                }
+                else
+                {
+                    PlaySound("StepForward");
+                }
+
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (null != anim)
+            {
+                if (currentIndex == 2)
+                {
+                    PlaySound("HandLeft");
+                }
+                else
+                {
+                    PlaySound("StepLeft");
+                }
+
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (null != anim)
+            {
+                if (currentIndex == 2)
+                {
+                    PlaySound("HandLower");
+                }
+                else if (currentIndex == 3)
+                {
+                    PlaySound("ElbowLower");
+                }
+                else
+                {
+                    PlaySound("StepBackward");
+                }
+
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (null != anim)
+            {
+                if (currentIndex == 2)
+                {
+                    PlaySound("HandRight");
+                }
+                else
+                {
+                    PlaySound("StepRight");
+                }
+
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (null != anim)
+            {
+                int randInt = Random.Range(0, 6);
+                if (randInt == 0)
+                {
+                    PlaySound("Pos1");
+                }
+                else if (randInt == 1)
+                {
+                    PlaySound("Pos2");
+                }
+                else if (randInt == 2)
+                {
+                    PlaySound("Pos3");
+                }
+                else if (randInt == 3)
+                {
+                    PlaySound("Pos4");
+                }
+                else
+                {
+                    PlaySound("Pos5");
+                }
             }
         }
 
